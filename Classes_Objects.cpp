7c1330412e166e5d0bdacb84d40f5caa6de7c855
Jay Patil <jayvardhanpatil123@gmail.com>
2021-11-11 17:50:54 +0530

    final commit

--- Classes_Objects.cpp
+++ Classes_Objects.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+using namespace std;
+class Employee{ 
+public:   
+    string Name;
+    string Company;
+    int Age;
 
-class Employee{
+    Employee(string name,string company,int age){ // Constructer
+        Name = name;
+        Age = age;
+        Company = company;
+    }
+
+    void IntroduceYourself(){
+        cout<<"Hi I am "<<Name<<"\n";
+        cout<<"I am "<<Age<<" years old\n";
+        cout<<"I work at "<<Company<<"\n";
+    }
 
 };
 
 int main(){
+    Employee emp1 = Employee("Jay","Google",21);//Varible of type Employee
+    Employee emp2 = Employee("Gary","Meta",33);
+    emp1.IntroduceYourself();
+    emp2.IntroduceYourself();
     return 0;
 }
